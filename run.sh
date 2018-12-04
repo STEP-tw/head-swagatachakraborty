bd4154b2c175fd61c8e93caaa6790aeac1296351
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-12-04 20:03:45 +0530

    Add two more files : file1, file2
    Add shell script : run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+node ./head.js -n5 file1
+node ./head.js -n 5 file1
+node ./head.js -5 file1
+node ./head.js file1 file2
+node ./head.js -n 5 file1 file2
+node ./head.js -n5 file1 file2
+node ./head.js -5 file1 file2
+node ./head.js -c5 file1
+node ./head.js -c 5 file1
+node ./head.js -c5 file1 file2
+node ./head.js -c 5 file1 file2
