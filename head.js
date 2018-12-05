f7a08a41fdf942b7187d2f298b1bdd3076c0e06d
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-12-05 12:44:08 +0530

    Modify : head.js

--- head.js
+++ head.js
@@ -1,9 +1,10 @@
 const { readFileSync } = require('fs'); 
 const { apply } = require('./src/util.js');
-const { extractDetails, formatContents, fetchContents } = require('./src/lib.js');
+const { parse, formatContents, fetchContents, getFilterFunction } = require('./src/lib.js');
 
 const main = function() {
-  let { files, filterContents, length } = extractDetails(process.argv);
+  let { files, type, length } = parse(process.argv);
+  let filterContents = getFilterFunction(type);
   let contents = apply(readFileSync, files);
   contents = fetchContents(filterContents, contents, length);
   console.log( formatContents(contents, files) );
