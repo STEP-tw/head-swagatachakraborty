bc617e05d40d512147b585c8ba7f586e126ee3aa
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2018-12-08 20:16:27 +0530

    Introduce : isOnlyLengthProvided, areTypeAndLengthGivenSeparately, areTypeAndLengthGivenTogather

--- src/lib.js
+++ src/lib.js
@@ -28,18 +28,30 @@ const fetchContents = function( filterContents, contents, length ) {
 
 const parse = function(details) {
   //for -5 f1 f1
-  if( details[0].startsWith('-') && details[0][1].match(/[0-9]/) ) {
+  if( isOnlyLengthProvided(details[0]) ) {
     return { files : details.slice(1), length : details[0].slice(1), type : '-n' };
   }
   //for -n 5 f1 f1
-  if(details[0].startsWith('-') && details[0][1].match(/[A-z]/) && details[0].length == 2) {
+  if( areTypeAndLengthGivenSeparately(details[0]) ) {
     return { files : details.slice(2), length : details[1], type : details[0] };
   }
   //for -n5 f1 f1
-  if(details[0].startsWith('-') && details[0][1].match(/[A-z]/)) {
+  if( areTypeAndLengthGivenTogather(details[0]) ) {
     return { files : details.slice(1), length : details[0].slice(2), type : details[0].slice(0,2) };
   }
   return { files : details.slice(), length : '10', type : '-n' };
+}
+
+const isOnlyLengthProvided = function(givenData) {
+  return givenData.startsWith('-') && givenData[1].match(/[0-9]/);
+}
+
+const areTypeAndLengthGivenSeparately = function(givenData) {
+  return givenData.startsWith('-') && givenData[1].match(/[A-z]/) && givenData.length == 2;
+}
+
+const areTypeAndLengthGivenTogather = function(givenData) {
+  return givenData.startsWith('-') && givenData[1].match(/[A-z]/);
 }
 
 const getFilterFunction = function(type) {
