3f665e9210f420a3bf5130df125e1b0988334f29
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2018-12-15 09:22:03 +0530

    Pretify lib.js format.js

--- src/format.js
+++ src/format.js
@@ -1,24 +1,24 @@
-const { missingFileError } = require('./Error');
+const { missingFileError } = require("./Error");
 
 const createHeading = function(title) {
-    return '==> ' + title + ' <==';
-  }
-  
-  const addHeading = function(context, files) {
-    let headings = files.slice();
-    return function(body) {
-      if(body == null) return missingFileError(headings.shift())[context] ;
-      return createHeading(headings.shift()) + '\n' + body;
-    };
-  }
-  
-  const formatContents = function(context, contents, files) {
-    if(files.length == 1 && contents[0] != null) return contents.join();
-    return contents.map( addHeading(context, files) ).join('\n\n');
-  }
+  return "==> " + title + " <==";
+};
 
-  module.exports = {
-      formatContents, 
-      addHeading,
-      createHeading
-  }
+const addHeading = function(context, files) {
+  let headings = files.slice();
+  return function(body) {
+    if (body == null) return missingFileError(headings.shift())[context];
+    return createHeading(headings.shift()) + "\n" + body;
+  };
+};
+
+const formatContents = function(context, contents, files) {
+  if (files.length == 1 && contents[0] != null) return contents.join();
+  return contents.map(addHeading(context, files)).join("\n\n");
+};
+
+module.exports = {
+  formatContents,
+  addHeading,
+  createHeading
+};
--- src/lib.js
+++ src/lib.js
@@ -1,71 +1,75 @@
-const { checkAndApply, toString, isNotNull } = require('./util');
-const { parse } = require('./parse');
-const { hasInvalidType, hasInvalidLength, lengthError, typeError } = require('./error');
-const { formatContents, addHeading, createHeading } = require('./format');
+const { checkAndApply, toString, isNotNull } = require("./util");
+const { parse } = require("./parse");
+const { hasInvalidType, hasInvalidLength, lengthError, typeError } = require("./error");
+const { formatContents, addHeading, createHeading } = require("./format");
 
 const getContents = function(context, length, type, files, isExist, reader) {
-  if(hasInvalidType(type)) return typeError(type)[context];
-  if(hasInvalidLength(length)[context]) return lengthError(length)[context][type];
+  if (hasInvalidType(type)) return typeError(type)[context];
+  if (hasInvalidLength(length)[context]) return lengthError(length)[context][type];
   let contents = checkAndApply(isExist, reader, files);
   contents = checkAndApply(isNotNull, toString, contents);
-  contents = fetchContents(getFilterFunction(type), contents, getBounds(length)[context]);
+  contents = fetchContents( getFilterFunction(type), contents, getBounds(length)[context] );
   return formatContents(context, contents, files);
-}
+};
 
-const getBounds = function (length) {
-  return { 
-    head : getHeadBounds(length),
-    tail : getTailBounds(length)
+const getBounds = function(length) {
+  return {
+    head: getHeadBounds(length),
+    tail: getTailBounds(length)
   };
-}
+};
 
 const getHeadBounds = function(length) {
-  return { lower : 0, upper : length };
-}
+  return { lower: 0, upper: length };
+};
 
 const getTailBounds = function(length) {
-  if(! +length) return {upper : length, lower : length};
-  return { lower : -Math.abs(length) };
-}
+  if (!+length) return { upper: length, lower: length };
+  return { lower: -Math.abs(length) };
+};
 
 const fetchNLines = function(bounds, content) {
-  if(!content) return content;
-  return content.split('\n')
-        .slice(bounds.lower, bounds.upper)
-        .join('\n');
-}
+  if (!content) return content;
+  return content
+    .split("\n")
+    .slice(bounds.lower, bounds.upper)
+    .join("\n");
+};
 
 const fetchNCharacters = function(bounds, content) {
-  if(!content) return content;
-  return content.split('')
-        .slice(bounds.lower, bounds.upper)
-        .join('');
-}
+  if (!content) return content;
+  return content
+    .split("")
+    .slice(bounds.lower, bounds.upper)
+    .join("");
+};
 
-const fetchContents = function( filterContents, contents, bounds ) {
-  return contents.map( filterContents.bind(null, bounds) );
-}
+const fetchContents = function(filterContents, contents, bounds) {
+  return contents.map(filterContents.bind(null, bounds));
+};
 
 const getFilterFunction = function(type) {
-  return (type == '-c') ? fetchNCharacters : fetchNLines;
-}
+  return type == "-c" ? fetchNCharacters : fetchNLines;
+};
 
-const getHead = getContents.bind(null, 'head');
-const getTail = getContents.bind(null, 'tail');
+const getHead = getContents.bind(null, "head");
+const getTail = getContents.bind(null, "tail");
 
-module.exports = { createHeading,
-                   addHeading,
-                   formatContents,
-                   fetchNLines,
-                   fetchNCharacters,
-                   parse,
-                   fetchContents,
-                   getFilterFunction,
-                   hasInvalidType,
-                   hasInvalidLength,
-                   getHead,
-                   lengthError,
-                   typeError,
-                   getHeadBounds,
-                   getTailBounds,
-                   getTail };
+module.exports = {
+  createHeading,
+  addHeading,
+  formatContents,
+  fetchNLines,
+  fetchNCharacters,
+  parse,
+  fetchContents,
+  getFilterFunction,
+  hasInvalidType,
+  hasInvalidLength,
+  getHead,
+  lengthError,
+  typeError,
+  getHeadBounds,
+  getTailBounds,
+  getTail
+};
